11ff675285c28431624838dff155bc98c8ba3942
Phuc <mdm.phuc2707@gmail.com>
2025-04-19 12:28:47 +0700

    update ca lam

---
+++ quanLyNhaThuocHuyPhuc/src/application/model/NhanVienByCa.java
@@ -0,0 +1,19 @@
+package application.model;
+
+public class NhanVienByCa {
+    private final String ca1;
+    private final String ca2;
+
+    public NhanVienByCa(String ca1, String ca2) {
+        this.ca1 = ca1;
+        this.ca2 = ca2;
+    }
+
+    public String getCa1() {
+        return ca1;
+    }
+
+    public String getCa2() {
+        return ca2;
+    }
+}
